9c95f814ac6e044f9089ba9b942eeb1e2f6120b3
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-08 14:25:40 +0530

    ADDED: function that returns mood string

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyzer {
+
+	public String analyzeMood(String message) {
+		
+		if(message.contains("sad")) {
+			return "SAD";
+		}
+		else {
+			return "HAPPY";
+		}
+	}
+}
