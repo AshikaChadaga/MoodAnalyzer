de3ac1311b29acdd639ab71ffa71b0125f34fbfb
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-09 15:37:13 +0530

    REFACTORED: try catch method catches NullPointerException

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,23 +1,27 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
-	
+
 	String message;
-	
+
 	public MoodAnalyzer() {
-		
+
 	}
-	
-	public MoodAnalyzer(String message){
+
+	public MoodAnalyzer(String message) {
 		this.message = message;
 	}
 
 	public String analyzeMood() {
-		
-		if(this.message.contains("Sad")) {
-			return "SAD";
-		}
-		else {
+
+		try {
+			if (this.message.contains("Sad")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} 
+		catch (NullPointerException e) {
 			return "HAPPY";
 		}
 	}
