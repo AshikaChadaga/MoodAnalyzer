32ea9389cde12fa9df7caac52c23b248c60d5461
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-09 22:43:56 +0530

    REFACTORED: method to test Null and Empty String

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -5,30 +5,44 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-	
+
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
-		
+
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad message");
 		String mood = moodAnalyzer.analyzeMood();
 		Assert.assertThat(mood, CoreMatchers.is("SAD"));
 	}
-	
+
 	@Test
 	public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
-		
+
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Happy message");
 		String mood = moodAnalyzer.analyzeMood();
 		Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
 	}
-	
+
 	@Test
 	public void givenMessage_WhenNull_ShouldReturnHappy() {
-		
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-		String mood = moodAnalyzer.analyzeMood();
-		Assert.assertEquals("HAPPY",mood);
 
-		
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+		try {
+			moodAnalyzer.analyzeMood();
+		} catch (MoodAnalyzerException e) {
+			Assert.assertEquals(MoodAnalyzerException.ExceptionType.ENTERED_NULL, e.type);
+		}
+
 	}
+
+	@Test
+	public void givenMessage_WhenEmpty_ShouldReturnHappy() {
+
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("");
+		try {
+			moodAnalyzer.analyzeMood();
+		} catch (MoodAnalyzerException e) {
+			Assert.assertEquals(MoodAnalyzerException.ExceptionType.ENTERED_EMPTY, e.type);
+		}
+	}
+
 }
